15a80bf1cc4eb7481915c86334d4c1c44d8bce24
numangit <abdulnumanjob@gmail.com>
2023-03-26 15:44:30 +0600

    interval added to call the function

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const countdownEl = document.getElementById("countdown");
 const initialMinutes = 10;
 let initialSeconds = initialMinutes * 60;
 
+//interval to call the function
+setInterval(updateCountdown, 1000);
+
 //function to countdown
 const updateCountdown = () => {
     const minutes = Math.floor(initialSeconds / 60);
