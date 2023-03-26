af0418435cc57262444111b573ae82c0f11fceac
numangit <abdulnumanjob@gmail.com>
2023-03-26 15:42:54 +0600

    function to countdown added

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 //elements
 const countdownEl = document.getElementById("countdown");
 
-
 //timer details
 const initialMinutes = 10;
 let initialSeconds = initialMinutes * 60;
 
-const
+//function to countdown
+const updateCountdown = () => {
+    const minutes = Math.floor(initialSeconds / 60);
+    let seconds = time % 60;
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    initialSeconds--;
+};
