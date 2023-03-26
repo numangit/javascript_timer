b2630942f672842e62fd929c82ccc781663b7400
numangit <abdulnumanjob@gmail.com>
2023-03-26 15:37:42 +0600

    elements and timer details added in js file

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello");
+//elements
+const countdownEl = document.getElementById("countdown");
+
+
+//timer details
+const initialMinutes = 10;
+let initialSeconds = initialMinutes * 60;
+
+const
