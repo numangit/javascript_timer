7a863d3b36806e589ca7e98fb5ae3eb5defcf0e9
numangit <abdulnumanjob@gmail.com>
2023-03-26 15:50:30 +0600

    timer is functional

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ const countdownEl = document.getElementById("countdown");
 const initialMinutes = 10;
 let initialSeconds = initialMinutes * 60;
 
-//interval to call the function
-setInterval(updateCountdown, 1000);
-
 //function to countdown
 const updateCountdown = () => {
     const minutes = Math.floor(initialSeconds / 60);
-    let seconds = time % 60;
+    let seconds = initialSeconds % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
 
     countdownEl.innerHTML = `${minutes}: ${seconds}`;
     initialSeconds--;
 };
+
+//interval to call the function
+setInterval(updateCountdown, 1000);
